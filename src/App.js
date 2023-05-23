c12a523f978152d68c31c0d1723807e308c80ba8
RBrunoChV <edu.rz@hotmail.com>
2023-05-22 20:49:05 -0500

    head added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import OverviewFlow from './Components/MainFlow/MainFlow.component'
+import React from "react";
+import OverviewFlow from "./Components/MainFlow/MainFlow.component";
+import './App.css';
+
 function App() {
   return (
-    <div style={{width: "100vw", height: "100vh"}}>
-      <OverviewFlow/>
+    <div style={{ width: "100%" }}>
+      <div
+        style={{
+          width: "100%",
+          backgroundColor: "#f9c42c",
+          height: "100vh",
+          paddingLeft: "40px",
+        }}
+      >
+        <span className="head-title">DIJKSTRA</span>
+        <br/>
+        <span className="head-subtitle">Algorithm - 1956</span>
+      </div>
+      <div style={{ width: "100%", height: "100vh" }}>
+        <OverviewFlow />
+      </div>
     </div>
   );
 }
