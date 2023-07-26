9e0bb4783271f03fb1fea15045b918d8ab2efd3f
RBrunoChV <edu.rz@hotmail.com>
2023-07-25 21:41:36 -0500

    home-sec init

--- src/App.js
+++ src/App.js
@@ -1,52 +1,37 @@
 import React from "react";
-import OverviewFlow from "./Components/MainFlow/MainFlow.component";
-import EdgerPortrait from "./assets/imgs/EdgerPortrait.png";
 import "./App.css";
-import LanguageSelector from "./Components/LanguageSelector/LanguageSelector";
-import { useTranslation } from 'react-i18next'
+// import { useTranslation } from 'react-i18next'
+// import LanguageSelector from "./Components/LanguageSelector/LanguageSelector";
 
 function App() {
-  const { t } = useTranslation();
+  // const { translate } = useTranslation();
   return (
     <div>
-      <div className="head-container">
-        <LanguageSelector/>
-        <span className="head-container-title">{t("head.title")}</span>
-        <br />
-        <span className="head-container-subtitle">{t("head.subtitle")}</span>
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            className="head-container-arrow-svg"
-          >
-            <path d="M12 1.993C6.486 1.994 2 6.48 2 11.994c0 5.513 4.486 9.999 10 10 5.514 0 10-4.486 10-10s-4.485-10-10-10.001zm0 18.001c-4.411-.001-8-3.59-8-8 0-4.411 3.589-8 8-8.001 4.411.001 8 3.59 8 8.001s-3.589 8-8 8z"></path>
-            <path d="M13 8h-2v4H7.991l4.005 4.005L16 12h-3z"></path>
-          </svg>
+      <div className="sec-container home-sec">
+        <div className="home-nav">
+          <span className="logo">EDURZ</span>
+          <span className="contact-btn">CONTACT ME</span>
         </div>
-        <br />
-      </div>
-      <div className="intro-container">
-        <div className="intro-container-img-bg">
-          <img src={EdgerPortrait} style={{ height: "100%" }} />
-        </div>
-        <div className="intro-container-one">
-          <div className="intro-container-card">
-            <span className="title">{t("intro.card-one.title")}</span>
-            <br />
-            <span className="desc">{t("intro.card-one.desc")}</span>
-          </div>
+        <div className="home-content">
+          <span className="title">DIJKSTRA ALGORITHM</span>
+          <span className="description">A Web Page for understanding the concepts</span>
+          <span className="description">and uses of Dijkstra Algorithm</span>
           <br/>
-          <div className="intro-container-card">
-            <span className="title">{t("intro.card-two.title")}</span>
-            <br />
-            <span className="desc">{t("intro.card-two.desc")}</span>
-          </div>
+          <br/>
+          <br/>
+          <br/>
+          <span className="a">PUB. IN</span>
+          <span className="b">1956</span>
         </div>
-        <div className="intro-container-two"></div>
       </div>
-      <div style={{ width: "100%", height: "100vh" }}>
-        <OverviewFlow />
+      <div className="sec-container description-sec">
+
+      </div>
+      <div className="sec-container algorithm-sec">
+
+      </div>
+      <div className="sec-container about-sec">
+
       </div>
     </div>
   );
