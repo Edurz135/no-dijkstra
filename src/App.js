307caa5b33bf5f8e9cf956f14d2b01e157c9b8d8
Eduardo Ramón <edu.rz@hotmail.com>
2023-07-31 08:13:35 -0500

    home-section

--- src/App.js
+++ src/App.js
@@ -35,41 +35,67 @@ function App() {
         <div className="bg-layer">
           <img src="/EdgerPortraitWithoutMouth.png" />
         </div>
+        <div className="bg-layer mouth">
+          <img src="/EdgerPortraitMouth.png" />
+        </div>
+        <div className="bg-layer words">
+          <span>I’M</span>
+          <span>DIJKSTRA</span>
+        </div>
       </div>
       <div className="sec-container description-sec container-fluid">
         <CustomNavbar />
         <div className="row align-items-center">
           <div className="row">
             <div className="col-md-6">
-              <CustomCard title="ORIGIN" description="Created by Edsger Dijkstra on 1956" />
+              <CustomCard
+                title="ORIGIN"
+                description="Created by Edsger Dijkstra on 1956"
+              />
             </div>
             <div className="col-md-6">
-              <CustomCard title="ALGORITHM" description="An algorithm for finding the shortest paths between nodes in a weighted graph" />
+              <CustomCard
+                title="ALGORITHM"
+                description="An algorithm for finding the shortest paths between nodes in a weighted graph"
+              />
             </div>
             <div className="col-md-6">
-              <CustomCard title="IMPORTANCE" description="Most influential algorithms in the history of computer science and graph theory. Had a significant impact on numerous fields of study and technologies that we use today." />
+              <CustomCard
+                title="IMPORTANCE"
+                description="Most influential algorithms in the history of computer science and graph theory. Had a significant impact on numerous fields of study and technologies that we use today."
+              />
             </div>
             <div className="col-md-6">
-              <CustomCard title="APPLICATIONS" description="Applied on route planning in maps, routing protocols in computer networks, analysis of transportation, among others." />
+              <CustomCard
+                title="APPLICATIONS"
+                description="Applied on route planning in maps, routing protocols in computer networks, analysis of transportation, among others."
+              />
             </div>
           </div>
         </div>
       </div>
-      <div className="sec-container algorithm-sec"><CustomNavbar /></div>
+      <div className="sec-container algorithm-sec">
+        <CustomNavbar />
+      </div>
       <div className="sec-container about-sec container-fluid">
         <CustomNavbar />
         <div className="row align-items-center">
           <div className="row row-lg-6 justify-content-center">
             <p className="title">I AM ON A MISSION TO PROVIDE AN</p>
-            <p className="title">EASILY <span className="title-variant">ACCESSIBLE PLATFORM</span></p>
-            <p className="description">My mission is to provide an easily accessible platform for students to comprehend the workings of Dijkstra's algorithm in a simpler and more straightforward manner.</p>
+            <p className="title">
+              EASILY <span className="title-variant">ACCESSIBLE PLATFORM</span>
+            </p>
+            <p className="description">
+              My mission is to provide an easily accessible platform for
+              students to comprehend the workings of Dijkstra's algorithm in a
+              simpler and more straightforward manner.
+            </p>
             <CustomLink />
           </div>
         </div>
       </div>
-      <div>
-      </div>
-    </div >
+      <div></div>
+    </div>
   );
 }
 
