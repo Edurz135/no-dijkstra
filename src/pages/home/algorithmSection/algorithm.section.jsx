e26525882c623edfd590b3078c62bdb5da592c8b
Edurz135 <edu.rz@hotmail.com>
2023-12-04 22:28:31 -0500

    algorihtm link

--- src/pages/home/algorithmSection/algorithm.section.jsx
+++ src/pages/home/algorithmSection/algorithm.section.jsx
@@ -2,15 +2,22 @@ import React from "react";
 // import "./home.section.css";
 export default function AlgorithmSection() {
   return (
-    <div>
-      <section className="relative min-h-screen bg-cprimary" id="algorithm">
-        <div className="absolute h-full w-full p-20 z-50">
-          <div className="h-full flex flex-column justify-around">
-            <div>
-            </div>
+    <section className="h-screen bg-con-primary" id="algorithm">
+      <div className="h-full p-20 z-50 flex items-center justify-center">
+        <div>
+          <div className="flex flex-column align-center text-center font-fdecorated">
+            <a
+              className="text-con-secondary text-center text-2xl leading-none no-underline"
+              href="/editor"
+              target="_blank"
+            >
+              <p>TRY</p>
+              <p>DIJKSTRA</p>
+              <p>EDITOR</p>
+            </a>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
