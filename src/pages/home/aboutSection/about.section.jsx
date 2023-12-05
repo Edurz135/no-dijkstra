56636f39d47f2c671d4769d51dce9718ea5a2aa7
Edurz135 <edu.rz@hotmail.com>
2023-12-04 21:27:04 -0500

    about section finished

--- src/pages/home/aboutSection/about.section.jsx
+++ src/pages/home/aboutSection/about.section.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import CustomLink from "../../../Components/CustomLink/CustomLink";
 // import "./home.section.css";
 export default function AboutSection() {
   return (
     <div>
-      <section className="relative min-h-screen bg-cprimary" id="about">
-        <div className="absolute h-full w-full p-20 z-50">
-          <div className="h-full flex flex-column justify-around">
-            <div>
+      <section className="bg-con-primary" style={{height: "90vh"}} id="about">
+        <div className="h-full p-20 z-50 flex items-center">
+          <div>
+            <div className="h-full flex flex-column align-center text-center">
+              <div className="font-fheader text-6xl text-con-secondary">
+                <p>I AM ON A MISSION TO PROVIDE AN</p>
+                <p>
+                  EASILY{" "}
+                  <span className="text-csecondary">ACCESSIBLE PLATFORM</span>
+                </p>
+              </div>
+              <p className="font-ftext text-3xl">
+                My mission is to provide an easily accessible platform for
+                students to comprehend the workings of Dijkstra's algorithm in a
+                simpler and more straightforward manner.
+              </p>
+              <div className="flex justify-center pt-20">
+                <CustomLink />
+              </div>
             </div>
           </div>
         </div>
